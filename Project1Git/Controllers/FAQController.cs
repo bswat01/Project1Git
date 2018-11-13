20cc4e580ebef93f69441e6908665cbb176cc2d5
bswat01 <beswat14@gmail.com>
2018-11-12 17:30:10 -0700

    sample styles

--- Project1Git/Controllers/FAQController.cs
+++ Project1Git/Controllers/FAQController.cs
@@ -20,20 +20,20 @@ namespace Project1Git.Controllers
         {
             if (ModelState.IsValid)
             {
-                ViewBag.California = "Mission name: Long Beach, California <br>" +
+                ViewBag.California = "Mission name: California Long Beach <br>" +
                     "Mission President's Name: Brian P. Patterson <br>" +
                     "Mission Address: 6500 E Atherton St, Long Beach, Ca, 90815 <br>" +
                     "Mission Language: English <br>" +
                     "Mission Climate: Mediterranean <br>" +
                     "Mission Dominate Religion: Christian <br>";
-                ViewBag.Canada = "Mission name: Calgary, Canada <br>" +
+                ViewBag.Canada = "Mission name: Canada Calgary <br>" +
                     "Mission President's Name: Stephen A. Keung <br>" +
                     "Mission Address: 7044 Farrell Rd SE, Calgary, AB T2H 0T2, Canada <br>" +
                     "Mission Language: English <br>" +
-                    "Mission Climate: humid continental<br>" +
+                    "Mission Climate: Humid Continental <br>" +
                     "Mission Dominate Religion: Christian <br>";
-                ViewBag.France = "Mission name: Lyon, France <br>" +
-                    "Mission President's Name: Jarom Barnes <br>" +
+                ViewBag.France = "Mission name: France Lyon <br>" +
+                    "Mission President's Name: Christophe Gérard Giraud-Carrier <br>" +
                     "Mission Address: Lyon Business Centre, 59 rue de l’Abondance, 69003 Lyon, France <br>" +
                     "Mission Language: French <br>" +
                     "Mission Climate: Humid Sub-tropical <br>" +
