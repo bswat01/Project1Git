805c490987f02d9c50478fa747786a581541b24e
bswat01 <beswat14@gmail.com>
2018-12-12 17:58:30 -0700

    classes and context

---
+++ Project1Git/Models/Missions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Project1Git.Models
+{
+    [Table("Missions")]
+    public class Missions
+    {
+        [Key]
+        public int missionID { get; set; }
+        public string missionName { get; set; }
+        public string missionPresidentName { get; set; }
+        public string missionAddress { get; set; }
+        public string Language { get; set; }
+        public string Climate { get; set; }
+        public string DominateReligion { get; set; }
+        public string Flag { get; set; }
+    }
+}
